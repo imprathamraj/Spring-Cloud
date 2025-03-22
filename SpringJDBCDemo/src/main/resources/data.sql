aa151f8bed862abbf1743fce89e1b9f982647bc3
imprathamraj <rjpratham05@gmail.com>
2025-03-22 13:07:19 +0530

    [Pratham Raj] Add. Commenting in data.sql

--- SpringJDBCDemo/src/main/resources/data.sql
+++ SpringJDBCDemo/src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- Default data
 insert into alien (id, name, tech) values (101,'Pratham', 'AI');
 insert into alien (id, name, tech) values (102,'Vivek', 'JS');
 insert into alien (id, name, tech) values (103,'Suraj', 'ML');
